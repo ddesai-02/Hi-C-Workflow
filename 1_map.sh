8d33965471d5472963dfceca24a05a12f2ad6fae
Devan Desai <93961382+ddesai-02@users.noreply.github.com>
2026-02-09 10:53:50 -0500

    Update 1_map.sh

--- 1_map.sh
+++ 1_map.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 #SBATCH --account=rrg-shaferab
-#SBATCH --mem=256G
-#SBATCH --cpus-per-task=32
-#SBATCH --time=4-23:59:00
+#SBATCH --mem=16G
+#SBATCH --cpus-per-task=1
+#SBATCH --time=2:59:00
 
 SAMPLE="Lakota"
 
-module load samblaster
 module load samtools
 module load bcftools
+module load picard
+module load bwa
 
 mkdir /home/devan/projects/def-shaferab/devan/Odocoileus_virginianus/sperm/HiC/$SAMPLE
 mkdir /home/devan/projects/def-shaferab/devan/Odocoileus_virginianus/sperm/HiC/$SAMPLE/bam_dir
@@ -16,46 +17,47 @@ mkdir /home/devan/projects/def-shaferab/devan/Odocoileus_virginianus/sperm/HiC/$
 mkdir /home/devan/projects/def-shaferab/devan/Odocoileus_virginianus/sperm/HiC/$SAMPLE/vcf_files/scaffolds
 mkdir /home/devan/projects/def-shaferab/devan/Odocoileus_virginianus/sperm/HiC/$SAMPLE/bam_dir/bam_scaffs
 
-REF="/home/devan/projects/def-shaferab/devan/Odocoileus_virginianus/fasteprr/ref_fasta/WTD_Ovbor_1.2.fna" # CHANGE IF SAMPLE IS A MD
+REF="/home/devan/projects/def-shaferab/devan/Odocoileus_virginianus/fasteprr/ref_fasta/WTD_Ovbor_1.2.fna" #CHANGE IF SAMPLE IS A MD
 RAW="/home/devan/projects/rrg-shaferab/DEER_FASTQ/SPERM/HiC/AS-11671/$SAMPLE"
 RAW2="/home/devan/projects/rrg-shaferab/DEER_FASTQ/SPERM/HiC2/AS-11671/$SAMPLE"
 MAIN="/home/devan/projects/def-shaferab/devan/Odocoileus_virginianus/sperm/HiC/$SAMPLE"
 BAMDIR="$MAIN/bam_dir"
 VCFDIR="$MAIN/vcf_files"
 
-# Align to a reference genome using bwa mem using the -5SP -T0 options, mark duplicates with samblaster
+# Align to a reference genome using bwa mem and importantly the -5SP -T0 options
 echo "beginning mapping for "$RAW/$SAMPLE"_R1.fastq.gz and "$RAW/$SAMPLE"_R2.fastq.gz"
-bwa mem -5SP -T0 "$REF" "$RAW/$SAMPLE"_R1.fastq.gz "$RAW/$SAMPLE"_R2.fastq.gz | samblaster | samtools view -S -h -b -F 2304 | samtools sort -@ 16 -n > "$BAMDIR/$SAMPLE".run1.bam
+bwa mem -5SP -T0 "$REF" "$RAW/${SAMPLE}_R1.fastq.gz" "$RAW/${SAMPLE}_R2.fastq.gz" | \
+samtools view -Sb | samtools sort -o "$BAMDIR/$SAMPLE.set1.sorted.bam"
 echo "mapping done for run 1"
 
-bwa mem -5SP -T0 "$REF" "$RAW2/$SAMPLE"_R1.fastq.gz "$RAW2/$SAMPLE"_R2.fastq.gz -o "$BAMDIR/$SAMPLE".run2.tmp.bam
-samblaster -i "$BAMDIR/$SAMPLE".run2.tmp.bam -o "$BAMDIR/$SAMPLE".run2.mrkdup.bam
-samtools view -S -h -b -F 2304 "$BAMDIR/$SAMPLE".run2.mrkdup.bam -o "$BAMDIR/$SAMPLE".run2.tmp2.bam
-samtools sort -@ 32 -n "$BAMDIR/$SAMPLE".run2.tmp2.bam -o "$BAMDIR/$SAMPLE".run2.bam
+echo "beginning mapping for "$RAW2/$SAMPLE"_R1.fastq.gz and "$RAW2/$SAMPLE"_R2.fastq.gz"
+bwa mem -5SP -T0 "$REF" "$RAW2/${SAMPLE}_R1.fastq.gz" "$RAW2/${SAMPLE}_R2.fastq.gz" | \
+samtools view -Sb - | samtools sort - -o "$BAMDIR/$SAMPLE.set2.sorted.bam"
 echo "mapping done for run 2"
 
-samtools merge -@16 -n -o "${BAMDIR}/${SAMPLE}.merged.bam" "${BAMDIR}/${SAMPLE}.run1.bam" "${BAMDIR}/${SAMPLE}.run2.bam"
-echo "merging done"
-
-samtools sort -@ 16 -o "${BAMDIR}/${SAMPLE}.merged.sorted.bam" "${BAMDIR}/${SAMPLE}.merged.bam"
-samtools index "${BAMDIR}/${SAMPLE}.merged.sorted.bam"
+# Merge and mark duplicates with picard
+echo "merging and marking duplicates"
+java -jar $EBROOTPICARD/picard.jar MarkDuplicates \
+      I="$BAMDIR/$SAMPLE.set1.sorted.bam" \
+      I="$BAMDIR/$SAMPLE.set2.sorted.bam" \
+      O="$BAMDIR/$SAMPLE.merged.mkdup.bam" \
+      M="$BAMDIR/$SAMPLE.metrics.txt" \
+      CREATE_INDEX=true
 
 # Call variants directly using the Hi-C reads with bcftools.
 # (Stage 1)
-bcftools mpileup --count-orphans -Ou -f $REF "${BAMDIR}/${SAMPLE}.merged.sorted.bam" -o "${VCFDIR}/$SAMPLE.mpileup.bcf"
+bcftools mpileup --count-orphans -Ou -f $REF "${BAMDIR}/$SAMPLE.merged.mkdup.bam" -o "${VCFDIR}/$SAMPLE.mpileup.bcf"
 # (Stage 2)
 bcftools call --threads 8 -mv -Oz "${VCFDIR}/$SAMPLE.mpileup.bcf" -o "${VCFDIR}/$SAMPLE.unfiltered.vcf.gz"
-# (Stage 3) - Index
-tabix "${VCFDIR}/$SAMPLE.unfiltered.vcf.gz"
-
-# Define your input VCF file
+bcftools index "${VCFDIR}/$SAMPLE.unfiltered.vcf.gz"
+# Define input VCF file
 INPUT_VCF="${VCFDIR}/$SAMPLE.unfiltered.vcf.gz" 
 
 # Loop through each scaffold name in the list file
 while IFS= read -r SCAFFOLD_ID; do
   
   # Set the output filename
-  OUTPUT_VCF="${VCFDIR}/scaffolds/${SCAFFOLD_ID}.vcf.gz"
+  OUTPUT_VCF="${VCFDIR}/scaffolds/${SCAFFOLD_ID}.vcf"
   
   echo "Processing $SCAFFOLD_ID..."
   
@@ -78,7 +80,7 @@ while IFS= read -r SCAFFOLD_ID; do
   # Use samtools view to filter by region (Scaffold ID)
   # -b: Output in BAM format
   # -@ 4: Use 4 threads (adjust as needed for faster processing)
-  samtools view -b -@ 4 -o "$OUTPUT_BAM" "${BAMDIR}/$SAMPLE.merged.sorted.bam" "$SCAFFOLD_ID"
+  samtools view -b -@ 4 -o "$OUTPUT_BAM" "${BAMDIR}/$SAMPLE.merged.mkdup.bam" "$SCAFFOLD_ID"
   samtools index "$OUTPUT_BAM"
     
 done < WTD_L90_scaffs.list
